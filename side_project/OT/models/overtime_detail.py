34a3e973c04131146d58df78ffb0ba91abcb093b
Harvey <haiminh@MacBook-Pro-cua-Harvey.local>
2020-01-14 11:12:34 +0700

    side project second  commit

--- side_project/OT/models/overtime_detail.py
+++ side_project/OT/models/overtime_detail.py
@@ -6,12 +6,12 @@ class overtime_detail(models.Model):
     _description = "Chi tiết làm thêm"
     _rec_name = 'employee'
 
-    employee = fields.Many2one(comodel_name='hr.employee',string="Tên nhân viên")
-    department = fields.Many2one(comodel_name='hr.department',string='Phòng ban')
-    date = fields.Date(string='Ngày làm thêm',default=datetime.today())
+    employee = fields.Many2one(comodel_name='hr.employee',string="Tên nhân viên", required=True)
+    department = fields.Many2one(comodel_name='hr.department',string='Phòng ban', required=True)
+    date = fields.Date(string='Ngày làm thêm',default=datetime.today(), required=True)
     ot_rate = fields.Float(string='Tỷ lệ làm thêm')
-    ot_time = fields.Integer(string="Số giờ làm thêm", min='0.5',max='16')
-    reason = fields.Char(string="Lý do làm thêm", size=100)
+    ot_time = fields.Integer(string="Số giờ làm thêm", min='0.5',max='16',required=True)
+    reason = fields.Char(string="Lý do làm thêm", size=100, required=True)
     status = fields.Selection(string='Trạng thái',selection=[('draft','Nháp'),
                                                              ('wait','Chờ phê duyệt'),
                                                              ('done','Đã phê duyệt'),
--- side_project/OT/models/overtime_management.py
+++ side_project/OT/models/overtime_management.py
@@ -5,11 +5,11 @@ class overtime_management(models.Model):
     _description = "Phiếu làm thêm"
 
 
-    department = fields.Many2one(comodel_name='hr.department', string='Phòng ban')
-    date = fields.Date(string='Ngày làm thêm')
-    ot_rate = fields.Float("Tỷ lệ làm thêm")
+    department = fields.Many2one(comodel_name='hr.department', string='Phòng ban',required=True)
+    date = fields.Date(string='Ngày làm thêm',required=True)
+    ot_rate = fields.Float("Tỷ lệ làm thêm",required=True)
     ot_sum = fields.Float("Tổng thời gian làm thêm")
-    reason = fields.Char(string='Lý do làm thêm', size=100)
+    reason = fields.Char(string='Lý do làm thêm', size=100,required=True)
     detail = fields.One2many(comodel_name='overtime_detail', inverse_name='management', string="Chi tiết làm thêm")
     status = fields.Selection(string="Trạng thái",selection=[('draft', 'Nháp'),
                                                              ('done', 'Đã phê duyệt'),
